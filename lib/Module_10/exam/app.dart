94996ac309f512561f9d7176a48f4d281f4fde7a
Sujon Kumar <sujonkumar0808@gmail.com>
2025-08-02 23:55:47 +0600

    initial commit

--- lib/Module_10/exam/app.dart
+++ lib/Module_10/exam/app.dart
@@ -1,13 +1,13 @@
 
 import 'package:apps/Module_10/assignment.dart';
-import 'package:apps/Module_10/exam.dart';
+import 'package:apps/Module_10/exam/exam.dart';
 import 'package:apps/Module_10/widget/ownWidget.dart';
 import 'package:apps/module8/list_view.dart';
 import 'package:apps/module8/module_8_class2.dart';
 import 'package:apps/module9/class_1.dart';
 import 'package:apps/module9/class_2.dart';
 import 'package:flutter/material.dart';
-import '../module8/flutter_class_3.dart';
+import '../../module8/flutter_class_3.dart';
 
 class MyApp extends StatelessWidget{
 
